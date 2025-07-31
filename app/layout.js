dedab427c32c18ae02495c3dbb9c63424e0449c3
muhammadzulfadly <muhammadzulfadly@student.telkomuniversity.ac.id>
2025-07-31 20:02:06 +0700

    perbaikan

--- app/layout.js
+++ app/layout.js
@@ -2,19 +2,19 @@ import PropTypes from "prop-types";
 
 export const metadata = {
   title: "LimapoccoeDigital",
-  description: "Selamat Datang. Wesbite ini merupakan website resmi Pemerintahan Desa Limapoccoe. Adapun layana yang dimiliki yaitu Pengajuan surat, Pengaduan, Berita desa, dan Informasi desa",
+  description: "Selamat Datang. Website ini merupakan website resmi Pemerintahan Desa Limapoccoe. Adapun layanan yang dimiliki yaitu Pengajuan surat, Pengaduan, Berita desa, dan Informasi desa",
   icons: {
     icon: "/logo.png",
   },
   openGraph: {
     title: "LimapoccoeDigital",
-    description: "Selamat Datang. Wesbite ini merupakan website resmi Pemerintahan Desa Limapoccoe. Adapun layana yang dimiliki yaitu Pengajuan surat, Pengaduan, Berita desa, dan Informasi desa",
+    description: "Selamat Datang. Website ini merupakan website resmi Pemerintahan Desa Limapoccoe. Adapun layanan yang dimiliki yaitu Pengajuan surat, Pengaduan, Berita desa, dan Informasi desa",
     url: "https://limapoccoedigital.id",
     images: "/logo.png",
     type: "website",
   },
   keywords: ["limapoccoe", "website limapoccoe", "aplikasi limapoccoe", "desa limapoccoe", "maros", "website maros", "limapoccoe digital"],
-  authors: [{ name: "Limapoccoe Digital" }],
+  authors: [{ name: "LimapoccoeDigital" }],
 };
 
 export default function RootLayout({ children }) {
