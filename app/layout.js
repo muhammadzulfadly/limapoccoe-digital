a27d40993400b4d21db36b5d789668b0c5d13643
muhammadzulfadly <muhammadzulfadly@student.telkomuniversity.ac.id>
2025-07-31 19:40:47 +0700

    perbaikan

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,30 @@
 import PropTypes from "prop-types";
 
 export const metadata = {
-  title: 'LimapoccoeDigital',
-  description: 'Website Resmi Desa Limmapocoe',
-}
+  title: "LimapoccoeDigital",
+  description: "Selamat Datang. Wesbite ini merupakan website resmi Pemerintahan Desa Limapoccoe. Adapun layana yang dimiliki yaitu Pengajuan surat, Pengaduan, Berita desa, dan Informasi desa",
+  icons: {
+    icon: "/logo.png",
+  },
+  openGraph: {
+    title: "LimapoccoeDigital",
+    description: "Selamat Datang. Wesbite ini merupakan website resmi Pemerintahan Desa Limapoccoe. Adapun layana yang dimiliki yaitu Pengajuan surat, Pengaduan, Berita desa, dan Informasi desa",
+    url: "https://limapoccoedigital.id",
+    images: "/logo.png",
+    type: "website",
+  },
+  keywords: ["limapoccoe", "website limapoccoe", "aplikasi limapoccoe", "desa limapoccoe", "maros", "website maros", "limapoccoe digital"],
+  authors: [{ name: "Limapoccoe Digital" }],
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="id">
       <body>{children}</body>
     </html>
-  )
+  );
 }
 
 RootLayout.propTypes = {
   children: PropTypes.node.isRequired,
 };
-
--- app/page.js
+++ app/page.js
@@ -1,24 +1,5 @@
 import { redirect } from "next/navigation";
-import Head from "next/head";
 
 export default function Home() {
-  // SEO: Set up the page metadata
-  return (
-    <>
-      <Head>
-        <title>Limapoccoe Digital</title>
-        <link rel="icon" type="image/png" href="https://limapoccoedigital.id/logo.png" />
-        <meta name="description" content="Website Resmi Pemerintahan Desa Limapoccoe" />
-        <meta name="keywords" content="limapoccoe, website limapoccoe, aplikasi limapoccoe, limapoccoedigital, desa limapoccoe, maros, website maros" />
-        <meta name="author" content="Limapoccoe Digital" />
-        <meta property="og:title" content="LimapoccoeDigital" />
-        <meta property="og:description" content="Website Resmi Pemerintahan Desa Limapoccoe" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://limapoccoedigital.id" />
-        <meta property="og:image" content="https://limapoccoedigital.id/logo.png" />
-      </Head>
-      {/* Redirect ke halaman /beranda */}
-      {redirect("/beranda")}
-    </>
-  );
+  redirect("/beranda");
 }
